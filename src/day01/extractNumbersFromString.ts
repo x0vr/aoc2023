ac29412900029cb81cc9ad9558a88fc8941deba2
x0v3rR <x0v3rr@gmail.com>
2023-12-04 20:34:56 +0100

    add Part1 solution as comment

--- src/day01/extractNumbersFromString.ts
+++ src/day01/extractNumbersFromString.ts
@@ -4,6 +4,9 @@ import { convertToNumbersString } from './convertNumberWords';
  * Function to get the first and the last numeric character combined from a provided string
  */
 export function extractNumbersFromString(line: string) {
+	// solution for Part 1:
+	//const numbers = line.replaceAll(/\D/g, '');
+	//
 	const numbers = convertToNumbersString(line);
 	const result = numbers[0] + numbers.split('').pop();
 	return result;
